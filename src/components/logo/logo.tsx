a7b937f4a0cd6cf1ef5e9074b147610e396b3866
devtubercode <devtubercode@gmail.com>
2024-10-23 01:12:40 -0500

    feat: update logo component

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -1,12 +1,30 @@
+import { portfolio } from "@/data";
 import styles from "./logo.module.css";
 
-export const Logo = () => {
+type LogoProps = {
+  withLogan?: boolean;
+  size?: string;
+};
+
+export const Logo = (props: LogoProps) => {
+  const { name, subLogan, src } = portfolio.header.info_logo;
   return (
-    <div className={styles.logo}>
-      <div className={styles.container_img}>
-        <img src="/assets/logo.png" alt="devTuberCode" />
-      </div>
-      <p>devTuberCode</p>
+    <div
+      className={styles.container}
+      style={{ gap: props.withLogan ? "16px" : 0 }}
+    >
+      <a href="summary">
+        <div className={styles.container_logo}>
+          <div
+            className={styles.container_img}
+            style={{ width: props.size || 50, height: props.size || 50 }}
+          >
+            <img src={src} alt={name} />
+          </div>
+          <p>{name}</p>
+        </div>
+      </a>
+      {props.withLogan && <p>{subLogan}</p>}
     </div>
   );
 };
